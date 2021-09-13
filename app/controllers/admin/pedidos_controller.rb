3ccb0905edd09457df164dac012cc95d2eb58744
César Osses <cesar.osses@gmail.com>
2021-09-13 14:35:13 -0300

    pagina admin pedidos

--- app/controllers/admin/pedidos_controller.rb
+++ app/controllers/admin/pedidos_controller.rb
@@ -1,19 +1,31 @@
 class Admin::PedidosController < Admin::AdminController
     
+    include Admin::PedidosHelper
+
+    before_action :asignar_pedido, only: [:mostrar, :editar, :actualizar]
 
     def listar
-        @lista_pedidos = Pedido.select(:codigo, :total, :created_at).order(created_at: :asc)
+        @lista_pedidos = Pedido.select(:id, :codigo, :total, :created_at).order(created_at: :desc)
     end
 
     def mostrar
-
+        #TODO mostar un pedido con todos los productos
     end
 
     def crear
-
+        #TODO mostrar formulario para crear pedido
     end
 
     def editar
+        #TODO editar pediddo existente
+        @datos_pedido = PedidosFormulario.new
+        @datos_pedido.id = @pedido.id
+        @datos_pedido.nombre = @pedido.datos_envio.nombre
+        @datos_pedido.correo = @pedido.datos_envio.correo
+        @datos_pedido.telefono = @pedido.datos_envio.telefono
+        @datos_pedido.direccion = @pedido.datos_envio.direccion
+        @datos_pedido.destino_id = @pedido.destino_id
+        @destinos = Destino.select(:id, :nombre).order(nombre: :asc)
 
     end
 
@@ -26,7 +38,7 @@ class Admin::PedidosController < Admin::AdminController
     end
 
     def eliminar
-
+        #TODO analizar si vale la pena
     end
 
     private
@@ -35,4 +47,10 @@ class Admin::PedidosController < Admin::AdminController
 
     end
 
+    def asignar_pedido
+        @pedido = Pedido.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        flash[:errors] = "Pedido no encontrado"
+        redirect_to action: :listar
+    end
 end
---
+++ app/helpers/admin/pedidos_helper.rb
@@ -0,0 +1,53 @@
+module Admin::PedidosHelper
+
+    class PedidosFormulario
+
+        include ActiveModel::Model
+        attr_accessor :id
+        attr_accessor :nombre
+        attr_accessor :correo
+        attr_accessor :direccion
+        attr_accessor :telefono
+        attr_accessor :destino_id
+
+        validates(:nombre, presence: true)
+        validates(:direccion, presence: true)
+        validates(:correo, presence: true)
+        validates(:telefono, presence: true)
+        validates(:destino_id, presence: true)
+
+        def persisted?
+            self.id != nil
+        end
+
+    end
+
+    # def crear_datos_envio(pedido)
+    #     DatosEnvio.create(
+    #         nombre: datos_formulario.nombre,
+    #         direccion: datos_formulario.direccion,
+    #         telefono: datos_formulario.telefono,
+    #         correo: datos_formulario.correo
+    #     )
+    # end
+
+    # def definir_pedido(total, pedido, datos_envio)
+    #     Pedido.new( 
+    #         total: total, 
+    #         destino_id: datos_formulario.destino_id,
+    #         estados_pedido: EstadosPedido.find_by(estado: 'solicitando'), 
+    #         datos_envio: datos_envio
+    #     )
+    # end
+
+    # def migrar_productos(carro, pedido)
+    #     carro.carros_contenidos.each do |contenido|
+    #         DetallesPedido.create(
+    #             pedido: pedido, 
+    #             producto: contenido.producto, 
+    #             cantidad: contenido.cantidad
+    #         )
+    #     end
+    # end
+
+end
